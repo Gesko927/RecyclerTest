e7d737ddfbedbcaaa0dad6f379bba04699a136b4
Gesko927 <gesko927@gmail.com>
2018-08-22 14:08:28 +0200

    Added RecyclerAdapter. Added Person model class.

--- feature/src/main/java/com/example/jetsoftpro/recyclertest/feature/MainActivity.java
+++ feature/src/main/java/com/example/jetsoftpro/recyclertest/feature/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerAdapter.ListItemClickListener {
+
+    private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerAdapter = new RecyclerAdapter(this);
     }
 
     @Override
@@ -48,5 +57,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void OnClick(Person selectedPerson) {
+
     }
 }
